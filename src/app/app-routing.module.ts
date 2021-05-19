2e6b6a6674043770d79e529b14bb77ef967b5bd4
Ẃeirdabyte <Alex.Thelander89@gmail.com>
2021-05-19 18:11:03 +0200

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputComponent } from './input/input.component';
+import { TextImageComponent } from './text-image/text-image.component';
+import { VideoComponent } from './video/video.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TextImageComponent},
+  { path: 'text-image', component: TextImageComponent},
+  { path: 'video', component: VideoComponent},
+  { path: 'input', component: InputComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routiongComponents = [TextImageComponent, VideoComponent, InputComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextImageComponent } from './text-image/text-image.component';
+import { VideoComponent } from './video/video.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextImageComponent,
+    VideoComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
